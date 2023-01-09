dc012f2054e6cb220d6c9669c34720ba3181197d
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-09 20:55:38 +0600

    got data in backend

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-app.get("/", (req, res) => res.send("Hello world"))
+app.use(cors())
+
+app.get("/", (req, res) => res.send({response: "Hello world"}))
 
 app.listen(port, () => console.log("listening to port", port))
