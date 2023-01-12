06bffc78e29b45748c269aab3f6636764e8ccc81
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-12 12:39:16 +0600

    uri given in env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv');
+require('dotenv').config()
 const cors = require('cors');
 const express = require('express');
 const app = express();
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://projects:xsScaYv4fYLP1gzo@cluster0.gvfpmor.mongodb.net/?retryWrites=true&w=majority`;
+const uri = process.env.URL;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
