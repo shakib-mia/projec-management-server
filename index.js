dfb45fbebe40e2ce5a214747dcdd3752b73d5756
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-08-01 11:40:19 +0600

    finding host link

--- index.js
+++ index.js
@@ -1,122 +1,140 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const cors = require('cors');
-const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-
 app.use(cors());
 app.use(express.json());
+// console.log();
 
-const client = new MongoClient(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(process.env.URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 client.connect();
 const collection = client.db("portfolio").collection("projects");
 
-
 // to deploy ========>> vercel --prod
 
-
 app.get("/projects", async (req, res) => {
-    const query = {};
-    const cursor = collection.find(query);
-    const projects = await cursor.toArray();
-    res.send(projects)
+  const query = {};
+  const cursor = collection.find(query);
+  const projects = await cursor.toArray();
+
+  //   console.log(req.get("host"));
+  res.send({ projects, host: req.get("host") });
 });
 
 app.get("/projects/:_id", async (req, res) => {
-    const query = {
-        _id: new ObjectId(req.params._id)
-    };
-    const cursor = collection.find(query);
-    const projects = await cursor.toArray();
-    res.send(projects);
+  const query = {
+    _id: new ObjectId(req.params._id),
+  };
+  const cursor = collection.find(query);
+  const projects = await cursor.toArray();
+  res.send(projects);
 });
 
 app.post("/projects", async (req, res) => {
-    const { title, smallDesc, liveSite, frontCode, hasBackendLink, backendLink, email, primaryImage, secondaryImage, tertiaryImage } = req.body;
+  const {
+    title,
+    smallDesc,
+    liveSite,
+    frontCode,
+    hasBackendLink,
+    backendLink,
+    email,
+    primaryImage,
+    secondaryImage,
+    tertiaryImage,
+  } = req.body;
 
-    const query = {
-        title: title,
-        liveSite: liveSite
-    }
-    const cursor = collection.find(query);
-    const exist = await cursor.toArray();
+  const query = {
+    title: title,
+    liveSite: liveSite,
+  };
+  const cursor = collection.find(query);
+  const exist = await cursor.toArray();
 
+  const data = {
+    title,
+    smallDesc,
+    liveSite,
+    frontCode,
+    backendLink,
+    primaryImage,
+    secondaryImage,
+    tertiaryImage,
+  };
 
-    const data = {
-        title, smallDesc, liveSite, frontCode, backendLink, primaryImage, secondaryImage, tertiaryImage
-    }
-
-
-    if (hasBackendLink) {
-        if (title && smallDesc && liveSite && frontCode && backendLink) {
-            if (exist.length) {
-                res.send({ message: "Site Already exists" })
-            } else {
-                const cursor = await collection.insertOne(data);
-                // const response = cursor.toArray();
-                res.send({ status: 200, message: "Adding the Site" })
-            }
-
-        } else {
-            res.send({ status: 204, message: "No field can be empty" })
-        }
+  if (hasBackendLink) {
+    if (title && smallDesc && liveSite && frontCode && backendLink) {
+      if (exist.length) {
+        res.send({ message: "Site Already exists" });
+      } else {
+        const cursor = await collection.insertOne(data);
+        // const response = cursor.toArray();
+        res.send({ status: 200, message: "Adding the Site" });
+      }
     } else {
-        if (title && smallDesc && liveSite && frontCode) {
-            if (exist.length) {
-                res.send({ message: "Site Already exists" })
-            } else {
-                const cursor = await collection.insertOne(data);
-                // const response = cursor.toArray();
-                res.send({ status: 200, message: "Adding the Site" })
-            }
-
-        } else {
-            res.send({ status: 204, message: "No field can be empty" })
-        }
+      res.send({ status: 204, message: "No field can be empty" });
     }
-
-})
+  } else {
+    if (title && smallDesc && liveSite && frontCode) {
+      if (exist.length) {
+        res.send({ message: "Site Already exists" });
+      } else {
+        const cursor = await collection.insertOne(data);
+        // const response = cursor.toArray();
+        res.send({ status: 200, message: "Adding the Site" });
+      }
+    } else {
+      res.send({ status: 204, message: "No field can be empty" });
+    }
+  }
+});
 
 app.put("/projects/:_id", async (req, res) => {
-    const { field, updatedDoc } = req.body;
-    const query = {
-        _id: new ObjectId(req.params._id)
-    }
+  const { field, updatedDoc } = req.body;
+  const query = {
+    _id: new ObjectId(req.params._id),
+  };
 
-    const updatedDocument = {
-        $set: {
-            [field]: updatedDoc
-        }
-    }
+  const updatedDocument = {
+    $set: {
+      [field]: updatedDoc,
+    },
+  };
 
-    const cursor = await collection.updateOne(query, updatedDocument);
-    if (cursor.modifiedCount) {
-        // res.send(cursor);
-        const newArr = [];
-        const upperCase = /([A-Z])/g;
-        const arr = field.split("");
-        arr.map(item => newArr.push(upperCase.test(item)))
+  const cursor = await collection.updateOne(query, updatedDocument);
+  if (cursor.modifiedCount) {
+    // res.send(cursor);
+    const newArr = [];
+    const upperCase = /([A-Z])/g;
+    const arr = field.split("");
+    arr.map((item) => newArr.push(upperCase.test(item)));
 
-        if (newArr.indexOf(true) > -1) {
-            arr.splice(newArr.indexOf(true), 0, " ");
-            const message = arr.join("") + " updated successfully"
-            res.send({ message, ...cursor });
-        }
+    if (newArr.indexOf(true) > -1) {
+      arr.splice(newArr.indexOf(true), 0, " ");
+      const message = arr.join("") + " updated successfully";
+      res.send({ message, ...cursor });
     }
-})
+  }
+});
 
 app.delete("/projects/:id", async (req, res) => {
-    const query = {
-        _id: new ObjectId(req.params.id)
-    }
-    const cursor = await collection.deleteOne(query);
-    res.send(cursor)
-})
+  const query = {
+    _id: new ObjectId(req.params.id),
+  };
+  const cursor = await collection.deleteOne(query);
+  res.send(cursor);
+});
 
+app.get("/", (req, res) =>
+  res.send({ message: "Welcome to Project Management Server" })
+);
 
-app.get("/", (req, res) => res.send({ message: "Welcome to Project Management Server" }));
-
-app.listen(port, () => console.log("listening", port))
+app.listen(port, () => console.log("listening", port));
